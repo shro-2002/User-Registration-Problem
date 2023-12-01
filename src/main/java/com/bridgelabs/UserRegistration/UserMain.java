7f66443a989045ad50173b1001079ef5f1f7ea9a
shro-2002 <96790181+shro-2002@users.noreply.github.com>
2023-12-01 08:16:07 +0530

    Added UC-10

--- src/main/java/com/bridgelabs/UserRegistration/UserMain.java
+++ src/main/java/com/bridgelabs/UserRegistration/UserMain.java
@@ -7,17 +7,17 @@ public class UserMain {
 	public static void main(String[] args) {
 		// UC-1
 		Scanner sc = new Scanner(System.in);
-//
-//		System.out.print("Enter the first name: ");
-//		String fName = sc.nextLine();
-//
-//		UserRegistration.validateFirstName(fName);
-//
-//		// UC-2
-//		System.out.print("Enter the Last name: ");
-//		String LName = sc.nextLine();
-//
-//		UserRegistration.validateLastName(LName);
+
+		System.out.print("Enter the first name: ");
+		String fName = sc.nextLine();
+
+		UserRegistration.validateFirstName(fName);
+
+		// UC-2
+		System.out.print("Enter the Last name: ");
+		String LName = sc.nextLine();
+
+		UserRegistration.validateLastName(LName);
 
 		// UC-3
 		System.out.print("Enter the Email of User: ");
@@ -26,16 +26,16 @@ public class UserMain {
 		UserRegistration.validateEmail(Email);
 
 		// UC-4
-//		System.out.print("Enter the Phone Number of User: ");
-//		String Phone = sc.nextLine();
-//
-//		UserRegistration.validatePhoneNumber(Phone);
-//
-//		// UC-7
-//		System.out.print("Enter the Password: ");
-//		String Pass = sc.nextLine();
-//
-//		UserRegistration.validatePassword(Pass);
+		System.out.print("Enter the Phone Number of User: ");
+		String Phone = sc.nextLine();
+
+		UserRegistration.validatePhoneNumber(Phone);
+
+		// UC-7
+		System.out.print("Enter the Password: ");
+		String Pass = sc.nextLine();
+
+		UserRegistration.validatePassword(Pass);
 
 	}
 }
